408652d4bca5bac141a80d4d7b0cdd28d8287ae5
juliadoriar <juliadoriarodrigues@gmail.com>
2024-11-19 22:55:40 +0000

    Insere a home page do membro da junta

---
+++ app/src/main/java/com/examples/digisocial/ui/view/home/HomePageJuntaView.kt
@@ -0,0 +1,77 @@
+package com.examples.digisocial.ui.view.home
+
+import LoginViewModel
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.examples.digisocial.ui.theme.DigiSocialTheme
+
+@Composable
+fun HomePageJuntaView(navController: NavController) {
+    val viewModel: LoginViewModel = viewModel()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(
+                    onClick = { /* INSERIR CAMINHO PARA RELATORIOS */ },
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .height(80.dp)
+                        .padding(8.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+                ) {
+                    Text("Relatórios")
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { viewModel.logout(onLogoutSuccess = { navController.navigate("login") }) },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.DarkGray)
+        ) {
+            Text("Logout")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewHomePageJuntaView() {
+    DigiSocialTheme {
+        HomePageJuntaView(navController = rememberNavController())
+    }
+}
